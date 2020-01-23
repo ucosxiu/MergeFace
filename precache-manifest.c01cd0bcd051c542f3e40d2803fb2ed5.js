3318da0ca72a81f63e6a59604775521549a2319a
GiorgioPeng <84867579@qq.com>
2020-01-23 02:16:12 +0000

    Deploying to gh-pages from master 3da1f34fac6000aa1411ea6b2321b79a2cf4bae4

---
+++ precache-manifest.c01cd0bcd051c542f3e40d2803fb2ed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07daee79767e96524e23194219520da",
+    "url": "/MergeFace/index.html"
+  },
+  {
+    "revision": "21163f2649d42ea57831",
+    "url": "/MergeFace/static/css/main.5c63a84f.chunk.css"
+  },
+  {
+    "revision": "8d8ac377b4a00926b4a9",
+    "url": "/MergeFace/static/js/2.bcc2ae80.chunk.js"
+  },
+  {
+    "revision": "f6e878851b73a206a29d2d05354fdf23",
+    "url": "/MergeFace/static/js/2.bcc2ae80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21163f2649d42ea57831",
+    "url": "/MergeFace/static/js/main.ee704de5.chunk.js"
+  },
+  {
+    "revision": "83332c87b22e4cd8cce1",
+    "url": "/MergeFace/static/js/runtime-main.28c0806d.js"
+  }
+]);
